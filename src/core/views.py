b266319b64224791dde200ec73db717141036864
Giovane Liberato <giovaneliberato@gmail.com>
2013-08-05 16:18:12 -0300

    adding authentication sample to app

--- src/core/views.py
+++ src/core/views.py
@@ -1,18 +1,36 @@
+import json
+import urllib
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 from core.models import *
 
 
 def home(request):
-    mensagens = Mensagem.objects.all()
-    values = {'mensagens': mensagens}
-    return render_to_response('core/home.html',
-        values,
-        context_instance = RequestContext(request))
+    if 'token' in request.GET:
+        token = request.GET['token']
+        ns = request.GET['ns']
+        url = "http://localhost:8081/api/auth/%s/%s" % (token, ns)
+        user_data = urllib.urlopen(url).read()
+        user_data = json.loads(user_data)
+        request.session.update({'user_data': user_data})
+    else:
+        user_data = request.session.get('user_data')
+
+    mensagens = Mensagem.objects.filter(user_id=user_data['user_id'])
+
+    values = {'mensagens': mensagens, 'user_data': user_data}
+
+    response = render_to_response('core/home.html',
+                                  values,
+                                  context_instance = RequestContext(request))
+
+    return response
 
 
 def nova_mensagem(request):
+    user_data = request.session.get('user_data')
     msg = Mensagem()
     msg.conteudo = request.POST['conteudo']
+    msg.user_id = user_data['user_id']
     msg.save()
     return redirect('/')
