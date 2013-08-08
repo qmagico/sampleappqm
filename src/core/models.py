938faf5b867b07dcf108b2982d856898ec7b5550
Tony Lampada <tonylampada@gmail.com>
2013-08-08 14:47:54 -0300

    deploy fix

--- src/core/models.py
+++ src/core/models.py
@@ -5,12 +5,12 @@ from django.db.models import fields
 
 class Mensagem(models.Model): 
     conteudo = models.TextField(null=False, blank=False)
-    user_id = fields.CharField(null=False)
+    user_id = fields.CharField(null=False, max_length=64)
 
 
 class Config(models.Model):
-    name = fields.CharField(null=False)
-    value = fields.CharField(null=False)
+    name = fields.CharField(null=False, max_length=64)
+    value = fields.CharField(null=False, max_length=512)
 
     @classmethod
     def get(cls, name, default_value=''):
